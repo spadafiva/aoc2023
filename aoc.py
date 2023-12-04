0e334fc7806a97102dc881a3d54d1d8c6cc64832
Joe Spadafora <joe.spadafora@onepeloton.com>
2023-12-03 22:26:09 -0500

    solves day 1 part 2

--- aoc.py
+++ aoc.py
@@ -1,19 +1,22 @@
 from utils.input_helpers import get_input_for as read_day
 import importlib
 
-# get day input and sample
-print("Day number:", end="")
-day = int(input())
+def run_day(day: int, sample: bool, part1: bool):
+    # # get day input and sample
+    # print("Day number:", end="")
+    # day = int(input())
 
-print("Use sample (y/n): ", end="")
-sample = bool(input() == "y")
+    # print("Use sample (y/n): ", end="")
+    # sample = bool(input() == "y")
 
-# get lines for the input
-lines = read_day(day, sample)
+    # get lines for the input
+    part_number = 1 if part1 else 2
+    lines = read_day(day, sample, part_number)
 
-# find the code for the day
-day_module = importlib.import_module(f"day{day:02}.part1")
-solve = getattr(day_module, 'solve')
+    # find the code for the day
+    part_number = '1' if part1 else '2'
+    day_module = importlib.import_module(f"day{day:02}.part{part_number}")
+    solve = getattr(day_module, 'solve')
 
-# run the solution
-solve(lines)
+    # run the solution
+    solve(lines)
--- utils/input_helpers.py
+++ utils/input_helpers.py
@@ -2,11 +2,11 @@ import os
 
 from config import ROOT_DIR
 
-def get_input_for(day: int, sample: bool) -> list[str]:
-    return [line for line in get_input_as_string_for(day, sample).splitlines()]
+def get_input_for(day: int, sample: bool, part: int) -> list[str]:
+    return [line for line in get_input_as_string_for(day, sample, part).splitlines()]
 
-def get_input_as_string_for(day: int, sample: bool) -> str:
-    input_file = _file_name(day, sample)
+def get_input_as_string_for(day: int, sample: bool, part: int) -> str:
+    input_file = _file_name(day, sample, part)
 
     with open(input_file) as file:
         content = file.read()
@@ -14,7 +14,7 @@ def get_input_as_string_for(day: int, sample: bool) -> str:
     content = content.rstrip("\n")
     return content
 
-def _file_name(day: int, sample: bool) -> str:
-    name = 'sample.txt'if sample  else 'input.txt'
+def _file_name(day: int, sample: bool, part: int) -> str:
+    name = f"sample{part}.txt" if sample  else 'input.txt'
     path = os.path.join(ROOT_DIR, f"day{day:02}", name)
     return path
